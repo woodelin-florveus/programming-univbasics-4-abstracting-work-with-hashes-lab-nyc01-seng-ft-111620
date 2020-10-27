133b8ea03c145fe9c7db4e4ea98e8668d1fb36c8
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-27 15:09:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,7 @@ end
 def update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
 
-  if hash[key] == nil
+  if hash[key] == hash[key]
     hash[key] == 1
   else
     hash[key] += 1
